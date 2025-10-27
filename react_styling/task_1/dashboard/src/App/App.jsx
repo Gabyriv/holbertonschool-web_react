63e82c3b96b1bbf342b44294a6e0ccdd0ad444c6
Gabriel <gabyrivm06@gmail.com>
2025-10-26 20:07:32 -0400

    Update CourseList and CourseListRow styles

--- react_styling/task_1/dashboard/src/App/App.jsx
+++ react_styling/task_1/dashboard/src/App/App.jsx
@@ -30,7 +30,7 @@ class App extends Component {
   };
 
   render() {
-    const { isLoggedIn = true } = this.props;
+    const { isLoggedIn = false } = this.props;
     const notificationsList = [
       {
         id: Date.now(),
