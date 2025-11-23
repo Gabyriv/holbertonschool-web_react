c27d91e5a5f056c17debdbadc09c65fed01e6b66
Gabriel <gabyrivm06@gmail.com>
2025-11-23 12:51:54 -0400

    Notificiations Slice

--- react_redux-part1/task_1/dashboard/src/app/rootReducer.js
+++ react_redux-part1/task_1/dashboard/src/app/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import notificationsReducer from '../features/notifications/notificationsSlice';
 
 /**
  * Root reducer combining all feature reducers
@@ -7,6 +8,7 @@ import authReducer from '../features/auth/authSlice';
  */
 const rootReducer = combineReducers({
   auth: authReducer,
+  notifications: notificationsReducer,
   // Add other feature reducers here as needed
 });
 
